3a2f8f6671d777db0718d619ef0e5de0e385205b
jacob <jachimaru@gmail.com>
2026-07-03 14:49:24 -0400

    added cosmetic stuff and random color button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,14 @@
 const container = document.querySelector("#container");
 const boxAttribute = "border: solid 1px black; background: white; height: 50px; width: 50px";
+let randomOn = false;
 let color = 'black';
 let gridAmount = 16
 const gridAmountSquared = Math.pow(gridAmount, 2);
 const dimensions = gridAmount * 50;
 container.setAttribute("style", "display: flex; flex-wrap: wrap; margin-inline: auto; width:"+ dimensions + "px");
 const btn = document.querySelector("#button");
+const randomButton = document.querySelector("#randomizer");
+randomButton.textContent = "Turn on Random Colors";
 btn.textContent = "Change Grid Size";
 btn.addEventListener("click", () => {
     let grid = prompt("Grid Size: ");
@@ -21,12 +24,24 @@ btn.addEventListener("click", () => {
     container.setAttribute("style", "display: flex; flex-wrap: wrap; margin-inline: auto; width:"+ newDimensions + "px");
     console.log(newBox)
     createBox(newBox)
-})
+});
+
+randomButton.addEventListener("click", () => {
+    if (!randomOn) {
+        randomOn = true;
+        randomButton.textContent = "Turn off Random Colors";
+        return randomOn
+    } else if (randomOn) {
+        randomOn = false;
+        randomButton.textContent = "Turn on Random Colors";
+        return randomOn
+    }
+});
 
 function chooseColor() {
     color = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0);
     return color;
-}
+};
 
 function createBox(num) {
     container.replaceChildren()
@@ -34,7 +49,11 @@ function createBox(num) {
         const box = document.createElement("div");
         box.setAttribute("style", boxAttribute);
         box.addEventListener("mouseenter", (e) => {
-            chooseColor();
+            if (randomOn) {
+                chooseColor();
+            } else if (!randomOn) {
+                color = 'black';
+            }
             e.target.style.background = color;
         });
         container.appendChild(box);
